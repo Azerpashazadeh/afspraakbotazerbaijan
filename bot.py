aedd2b120e0545dfe317c20518fdf14c80be9dc9
Azerpashazadeh <120445892+Azerpashazadeh@users.noreply.github.com>
2026-07-29 21:19:37 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -492,3 +492,5 @@ threading.Thread(target=opschoon_loop, daemon=True).start()
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+# payriff v1
